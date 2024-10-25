8a1bd96bc1f3c2d75916bec55a9f7bf2fc0a2b46
Oscar Hinton <145544306+BHASVIC-OscarHinton23@users.noreply.github.com>
2024-10-25 11:14:33 +0100

    Start implementing hoverBehaviour
    Doesn't work in this commit

---
+++ Assets/Scenes/SimulationSelection/hoverBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverBehaviour : MonoBehaviour
+{
+    public bool hovering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hovering = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkForHover();
+    }
+
+    // Check if mouse is hovering over panel, and update `hovering` accordingly.
+    void checkForHover()
+    {
+        Event currentEvent = Event.current;
+        Vector2 mousePosition = currentEvent.mousePosition;
+
+        Debug.Log($"Mouse information: X={mousePosition.x} Y={mousePosition.y}");
+    }
+}
