5774715aef6e3a2e0ee1f3aa44726c327b1e6392
Oscar Hinton <145544306+BHASVIC-OscarHinton23@users.noreply.github.com>
2024-10-25 11:34:46 +0100

    [Test] Mouse is inside of window
    Works fine

--- Assets/Scenes/SimulationSelection/hoverBehaviour.cs
+++ Assets/Scenes/SimulationSelection/hoverBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class hoverBehaviour : MonoBehaviour
@@ -21,8 +22,28 @@ public class hoverBehaviour : MonoBehaviour
     // Check if mouse is hovering over panel, and update `hovering` accordingly.
     void checkForHover()
     {
+        // Mouse position is relative to bottom-left of window, not screen
+        // (i.e 0,0 is the bottom left corner)
         Vector2 mousePosition = Input.mousePosition;
 
-        Debug.Log($"Mouse information: X={mousePosition.x} Y={mousePosition.y}");
+        // Information about the program's window
+        DisplayInfo info = Screen.mainWindowDisplayInfo;
+        int windowHeight = info.height;
+        int windowWidth = info.width;
+
+        // Checks for if the mouse is outside of the window area
+        // If either are negative, the mouse is DEFINITELY not inside the window's boundaries
+        // If mouse.X > window width, the mouse is outside.
+        // Likewise with mouse.Y > window height
+        if (mousePosition.x < 0 || mousePosition.y < 0 || mousePosition.x > windowWidth || mousePosition.y > windowHeight)
+        {
+            Debug.Log("Outside");
+            // Can just return here, no need to set hovering to false.
+            // (they can't just stop hovering and move the mouse that far, that quickly)
+            return;
+        }
+        Debug.Log("Inside");
+        
+        //Debug.Log($"Mouse information: X={mousePosition.x} Y={mousePosition.y}");
     }
 }
