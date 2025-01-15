800d7bbf1f9f19bf7262be8d7d46772739af3676
Oscar Hinton <145544306+BHASVIC-OscarHinton23@users.noreply.github.com>
2025-01-15 08:58:42 +0000

    Copy mass listeners

--- Assets/Scenes/Simulations/ProjectileMotiono/InformationPanelPM.cs
+++ Assets/Scenes/Simulations/ProjectileMotiono/InformationPanelPM.cs
@@ -45,4 +45,84 @@ public class InformationPanelPM : MonoBehaviour
         pmScript.angleOfProjection = angleSlider.GetComponent<Slider>().value;
         pmScript.velocity = velocitySlider.GetComponent<Slider>().value;
     }
+
+
+    #region Mass Listeners
+
+    // Update label stating the current value
+    public void updateMassLabel(float value)
+    {
+        GameObject slider = massPanel.transform.Find("Slider").gameObject;
+        GameObject textLabel = massPanel.transform.Find("VariableName").gameObject;
+        TextMeshProUGUI textComponent = textLabel.GetComponent<TextMeshProUGUI>();
+        Slider sliderComponent = slider.GetComponent<Slider>();
+
+        textComponent.text = $"Mass: {sliderComponent.value} kg";
+    }
+
+
+    // Listener for lower bound text input
+    public void changeMassLowerBound(string value)
+    {
+        GameObject lowerBound = massPanel.transform.Find("LowerBound").gameObject;
+        GameObject upperBound = massPanel.transform.Find("UpperBound").gameObject;
+        GameObject sliderObject = massPanel.transform.Find("Slider").gameObject;
+        Slider slider = sliderObject.GetComponent<Slider>();
+        TMP_InputField lower = lowerBound.GetComponent<TMP_InputField>();
+        TMP_InputField upper = upperBound.GetComponent<TMP_InputField>();
+
+        float lowerValue = float.Parse(lower.text);
+        float upperValue = float.Parse(upper.text);
+
+        // Check if value is greater than maximum, or if below 0
+        // Set to 0 and return if so
+        if (lowerValue >= upperValue || lowerValue <= 0)
+        {
+            lower.text = "0.001";
+            slider.minValue = 0.001f;
+
+            return;
+        }
+
+        // Change lower bound to what was inputted
+        slider.minValue = lowerValue;
+    }
+
+
+    // Listener for upper bound text input
+    public void changeMassUpperBound(string value)
+    {
+        GameObject lowerBound = massPanel.transform.Find("LowerBound").gameObject;
+        GameObject upperBound = massPanel.transform.Find("UpperBound").gameObject;
+        GameObject sliderObject = massPanel.transform.Find("Slider").gameObject;
+        Slider slider = sliderObject.GetComponent<Slider>();
+        TMP_InputField lower = lowerBound.GetComponent<TMP_InputField>();
+        TMP_InputField upper = upperBound.GetComponent<TMP_InputField>();
+
+        float lowerValue = float.Parse(lower.text);
+        float upperValue = float.Parse(upper.text);
+
+        // Check if value is greater than maximum, or if below 0
+        // Set to 0 and return if so
+        if (upperValue <= lowerValue)
+        {
+            upper.text = $"{lowerValue + 1}";
+            slider.minValue = lowerValue + 1;
+
+            return;
+        }
+
+        // Change lower bound to what was inputted
+        slider.maxValue = upperValue;
+    }
+
+    public void massSliderListener(float value)
+    {
+        DoCircularMotion cmComponent = body.GetComponent<DoCircularMotion>();
+        GameObject slider = massPanel.transform.Find("Slider").gameObject;
+        Slider sliderComponent = slider.GetComponent<Slider>();
+        cmComponent.mass = sliderComponent.value;
+    }
+    #endregion
+
 }
