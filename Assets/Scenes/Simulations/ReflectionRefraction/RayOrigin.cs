e59c2f02a355fdd5272f3da7eff92f57407dc35e
Oscar Hinton <oscar.hinton23@students.bhasvic.ac.uk>
2025-01-30 03:08:15 +0000

    Testing the drawing of the direction vector
    It is flipped the wrong way around

--- Assets/Scenes/Simulations/ReflectionRefraction/RayOrigin.cs
+++ Assets/Scenes/Simulations/ReflectionRefraction/RayOrigin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RayOrigin : MonoBehaviour
@@ -27,12 +28,18 @@ public class RayOrigin : MonoBehaviour
 
         // Rotate by angle
         // See https://docs.unity3d.com/Packages/com.unity.mathematics@1.3/manual/4x4-matrices.html
-        float cosAngle = math.cos(math.PI - this.angle);
-        float sinAngle = math.sin(math.PI - this.angle);
+        float cosAngle = math.cos(math.PI - math.radians(this.angle));
+        float sinAngle = math.sin(math.PI - math.radians(this.angle));
         float2x2 rotationMatrix = math.float2x2(cosAngle, -sinAngle, sinAngle, cosAngle);
 
         up = math.mul(rotationMatrix, up);
 
         Debug.Log($"Direction vector: ({up.x}, {up.y})");
+
+        // Debug, draw vector
+        LineRenderer lr = GetComponentInParent<LineRenderer>();
+
+        Vector3[] points = { origin, origin + up };
+        lr.SetPositions(points);
     }
 }
